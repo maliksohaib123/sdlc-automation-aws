0e02dcaaffa8b67ffbff926debe92fa0253189c5
Malik M Sohaib <161340451+maliksohaib123@users.noreply.github.com>
2024-03-09 02:58:58 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import http from "http";
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 80;
 
 const server = http.createServer(app);
 
